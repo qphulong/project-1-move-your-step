0d4c0573262c4dcc673e7ec21ff1e487b1c524da
qplong21 <qphulong@gmail.com>
2023-11-16 12:37:24 +0700

    level1 class
    Tính làm mỗi lv là 1 class trc, có j change sau cũng dc

---
+++ level1.py
@@ -0,0 +1,8 @@
+import floor
+
+class Level1:
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-# import tkinter as tk
-
-# class SquareDrawer:
-#     def __init__(self, master):
-#         self.master = master
-#         master.title("Simple Square Drawer")
-
-#         # Canvas to draw the square
-#         self.canvas = tk.Canvas(master, width=200, height=200)
-#         self.canvas.pack()
-
-#         # Button to trigger square drawing
-#         draw_button = tk.Button(master, text="Draw Square", command=self.draw_square)
-#         draw_button.pack()
-
-#     def draw_square(self):
-#         # Coordinates for the square (top-left and bottom-right)
-#         x1, y1 = 50, 50
-#         x2, y2 = 150, 150
-
-#         # Draw the square on the canvas
-#         self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill="blue")
-
-# # Create the main window
-# root = tk.Tk()
-# app = SquareDrawer(root)
-# root.mainloop()
-
 import floor
 import tkinter as tk
 
