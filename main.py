e4ffb8c47ebbf6a10f5b5757df1709b7c32fe46e
viettungvuong <viettungvuong@gmail.com>
2023-11-18 09:34:32 +0700

    Complete map GUI

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ root.title("Move Your Step")
 # Open the file in read mode
 with open('input/input1-level1.txt', 'r') as file:
     size_info = file.readline().strip().split(',')
-    m,n = map(int, size_info)
+    m, n = map(int, size_info)
 
     # Read the second line describing the floor index
     floor_info = file.readline().strip()
@@ -26,7 +26,7 @@ print(f"Floor Information: {floor_info}")
 board_size = 8  # Define the size of the board
 cell_size = 50  # Define the size of each cell in pixels
 
-canvas = tk.Canvas(root, width=board_size*cell_size, height=board_size*cell_size)
+canvas = tk.Canvas(root, width=board_size * cell_size, height=board_size * cell_size)
 canvas.pack()
 
 # Draw the board
@@ -37,12 +37,19 @@ for i in range(m):
         x2 = x1 + cell_size
         y2 = y1 + cell_size
 
-        canvas.create_rectangle(x1, y1, x2, y2, fill=("black" if map[i][j] == "-1" else "white"))
+        canvas.create_rectangle(
+            x1, y1, x2, y2,
+            fill=("black" if map[i][j] == "-1" else "white"),
+            outline="grey"  # Specify the color of the border (here, "grey")
+        )
 
         text_x = x1 + cell_size // 2
         text_y = y1 + cell_size // 2
-        canvas.create_text(text_x, text_y, text=("T" if map[i][j] == "T" else "A" if map[i][j] == "A" else ""), fill=("blue" if map[i][j] == "T" else "red" if map[i][j] == "A" else "white"), font=("Arial", 10))
+
+        canvas.create_text(text_x, text_y,
+                           text=(map[i][j] if map[i][j].__contains__("A") or map[i][j]=="T1" else "")
+                           , fill=("blue" if map[i][j] == "T1" else "red" if map[i][j].__contains__("A") else "white")
+                           , font=("Arial", 25))
 
 # Run the Tkinter main loop
 root.mainloop()
-
