88e3931588f5961893c494d9e554ebfec1d9ffb9
viettungvuong <viettungvuong@gmail.com>
2023-11-18 09:27:42 +0700

    Input text file

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Move Your Step")
 
-# input này để test thử
-m = int(input("Input m: "))
-n = int(input("Input n: "))
+# Open the file in read mode
+with open('input/input1-level1.txt', 'r') as file:
+    size_info = file.readline().strip().split(',')
+    m,n = map(int, size_info)
 
+    # Read the second line describing the floor index
+    floor_info = file.readline().strip()
 
-map = [[0 for _ in range(n)] for _ in range(m)]
+    # Read the grid layout
+    map = []
+    for _ in range(m):
+        line = file.readline().strip().split(',')
+        map.append(line)
 
-for i in range (0,m):
-    for j in range (0, n):
-        map[i][j]=input(f"Insert for {i},{j}: ")
+# Display the information read from the file
+print(f"Number of rows: {m}")
+print(f"Number of columns: {n}")
+print(f"Floor Information: {floor_info}")
 
 board_size = 8  # Define the size of the board
 cell_size = 50  # Define the size of each cell in pixels
@@ -22,14 +30,14 @@ canvas = tk.Canvas(root, width=board_size*cell_size, height=board_size*cell_size
 canvas.pack()
 
 # Draw the board
-for i in range(board_size):
-    for j in range(board_size):
+for i in range(m):
+    for j in range(n):
         x1 = j * cell_size
         y1 = i * cell_size
         x2 = x1 + cell_size
         y2 = y1 + cell_size
 
-        canvas.create_rectangle(x1, y1, x2, y2, fill=("black" if map[i][j] == "1" else "white"))
+        canvas.create_rectangle(x1, y1, x2, y2, fill=("black" if map[i][j] == "-1" else "white"))
 
         text_x = x1 + cell_size // 2
         text_y = y1 + cell_size // 2
