8b94baeab0d16ffb3249edde3e68777de59937b7
qplong21 <qphulong@gmail.com>
2023-11-28 10:25:08 +0700

    an example

--- level2_beta.py
+++ level2_beta.py
@@ -398,6 +398,7 @@ class SearchTree:
         print("No path found")
 
 searchTree2 = SearchTree()
-searchTree2.getInputFile("input//input3-level2.txt")
+
+searchTree2.getInputFile("input//input4-level2.txt")
 searchTree2.AStar()
 pass
